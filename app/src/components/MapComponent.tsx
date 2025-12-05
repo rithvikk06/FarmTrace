0acfb5a99a98317b27abf40a2b753d58d7213485
Siddhesh Songirkar <sid.songirkar@icloud.com>
2025-12-05 15:04:55 -0500

    map adjust

--- app/src/components/MapComponent.tsx
+++ app/src/components/MapComponent.tsx
@@ -27,7 +27,7 @@ const MapComponent: React.FC<MapComponentProps> = ({ onCoordsChange }) => {
   };
 
   return (
-    <MapContainer center={[51.505, -0.09]} zoom={5} style={{ height: '300px', width: '100%', borderRadius: '8px' }}>
+    <MapContainer center={[40, -60]} zoom={4} style={{ height: '500px', width: '100%', borderRadius: '8px' }}>
       <TileLayer
         url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
         attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
